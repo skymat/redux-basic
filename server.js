83323996f82928155cd721728cb0a059478f592e
Mathieu Bourque <skymatb@gmail.com>
2017-04-14 09:56:46 +0200

    body-parser

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var app = express();
 var ejs = require("ejs");
 app.set('view engine', 'ejs');
 
+//POST
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // this is used for parsing the JSON object from POST
+
 //Permet de spécifier que tout ce qu'on dépose dans ce répertoire public sera automatiquement accessible
 //On peut l'appeler comme on veut, pas que public
 app.use(express.static("public"));
